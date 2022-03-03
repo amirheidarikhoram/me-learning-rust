4a627240464ba6d8716c7eee95c8e131f37666a7
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-03-03 10:13:54 +0330

    String performance practice

--- string/src/main.rs
+++ string/src/main.rs
@@ -15,10 +15,28 @@ fn main() {
 
     // #######################################
     // string conctenation
+
+    // this approach is not that efficient beacause everytime line 23 gets evaluated there is a pair of deallocating and allocating happening.
     let words = ["Hello", "my", "name", "is", "Amir Hosein"];
     let mut join = String::new();
     for w in words{
         join = format!("{} {}", join, w);
     }
     println!("Result: {}", join);
+
+    // this is much better
+    let mut better_join = String::new();
+    for w in words{
+        better_join += w;
+    }
+
+    println!("The more efficient result: {}", better_join);
+
+    // in previos cases length of the array <words> was 5 and total characters count was 23 (occupying 23 bytes in UTF-8) so there happens 4 buffer allocations and we can make it better with createing a string with capacity of 24
+
+    let mut best_join = String::with_capacity(24);
+    for w in words {
+        best_join += w;
+    }
+    println!("The most efficient result: {} with capacity, {}", best_join, best_join.capacity());
 }
