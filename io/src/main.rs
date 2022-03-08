6f25e4183e63645ca956e8ee33b1d2350b28c669
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-03-09 00:13:27 +0330

    Practice file handling

--- io/src/main.rs
+++ io/src/main.rs
@@ -1,3 +1,5 @@
+use std::{io::{Read, Write}};
+
 fn main() {
     // // lets just get some arguments and print 'em
     // let command_line = std::env::args();
@@ -26,4 +28,17 @@ fn main() {
     // // unwrap returns the data inside the Ok enum option which is count of data read in bytes
     // let mut another_line = format!("");
     // println!("{}", std::io::stdin().read_line(&mut another_line).unwrap());
+
+    // it think it is a good time to work with files
+    let mut args = std::env::args();
+    args.next(); // first argument is executable name so we are ignoring it
+    let file_name = args.next().unwrap();
+    let mut data = format!("");
+    let mut file = std::fs::File::open(&file_name).unwrap();
+    file.read_to_string(&mut data).unwrap();
+    println!("Read data is: {}", &data);
+    // lets just copy it to another file
+    let new_file_name = format!("{}-new", file_name);
+    let mut new_file = std::fs::File::create(&new_file_name).unwrap();
+    new_file.write_all(data.as_bytes()).expect("Couldn't write to file");
 }
