200e1afa430e06083c2aa6222c4441d471f47d71
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-03-11 09:23:57 +0330

    Rust Recap Session 1 | Chapter 1..=3

---
+++ rust-recap/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    // comment
+    /* other type of comment */
+    /* /*
+     */ */
+    // print!("{} {}, {} \n", "Hello", "World", "!");
+
+    // // chapter 3
+    // let mut number = 2;
+    // let another_number = 5;
+    // println!("{} + {} = {}", number, another_number, number + another_number);
+    // number += 1;
+    // println!("{}", number)
+
+    // let _ = 2;
+    // println!("{}", _); // ERROR!
+
+    // let number1 = 12;
+    // {
+    //     let number1 = 21;
+    //     println!("{}", number1);
+    // }
+
+    // let mut _n = 1;
+    // _n = 2;
+    // let _n = 3.14;
+    // _n = 5.9; // ERROR!
+
+    println!("{} {}", "abcde".len(), str::len("abcde"));
+    
+}
