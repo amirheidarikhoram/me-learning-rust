14f5dcada07a9f86933ffcc7d38e471708f9a02e
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-03-17 00:02:19 +0330

    Add mutable app state

--- simple-server-actix/src/main.rs
+++ simple-server-actix/src/main.rs
@@ -1,27 +1,26 @@
 #[allow(unused)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+use std::sync::Mutex;
 struct AppState {
-    name: String
+    counter: Mutex<i32>,
 }
 
 #[get("/")]
-async fn index (data: web::Data<AppState>) -> impl Responder {
-    format!("{}", &data.name)
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Ne value: {}", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .app_data(web::Data::new(
-            AppState {
-                name: "A simple web app in actix".to_string()
-            }
-        ))
-        .service(index)
-    })
-    .bind(("127.0.0.1", 5000))?
-    .run()
-    .await
+    let counter = web::Data::new(AppState {
+        counter: Mutex::new(0),
+    });
+    HttpServer::new(move || {
+            App::new().app_data(counter.clone()).service(index)
+        })
+        .bind(("127.0.0.1", 5000))?
+        .run()
+        .await
 }
