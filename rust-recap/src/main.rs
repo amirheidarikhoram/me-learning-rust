975f50d44d3763f39bec8d8cfbf8e998b67e0c60
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-03-18 09:38:34 +0330

    Rust recap session 2

--- rust-recap/src/main.rs
+++ rust-recap/src/main.rs
@@ -26,6 +26,43 @@ fn main() {
     // let _n = 3.14;
     // _n = 5.9; // ERROR!
 
-    println!("{} {}", "abcde".len(), str::len("abcde"));
-    
+    // println!("{} {}", "abcde".len(), str::len("abcde"));
+
+    // chapter 4
+
+    // let n = 4;
+    // print!(
+    //     "{:?}",
+    //     if n > 1000 {
+    //         "big"
+    //     } else if n > 0 {
+    //         "small"
+    //     } else if n < 0 {
+    //         "negative"
+    //     } else {
+    //         "neither positive nor negative"
+    //     }
+    // );
+
+    // ERROR
+    // let val = if n > 5 {"big"} else {4};
+
+    // for n in 1..=11 {
+    //     println!("{}", n)
+    // }
+
+    // let index = 8;
+    // for index in 0..4 {
+    //     print!("{} ", index);
+    // }
+    // print!(":{}", index);
+
+    // let numbers = [1, 2, 3, 4, 5];
+    // let index = 0;
+    // println!("{}", numbers[index]);
+
+    // println!("{}", std::mem::size_of::<i32>())
+
+    // #[allow(unused_variables)]
+    // let number = 32;
 }
