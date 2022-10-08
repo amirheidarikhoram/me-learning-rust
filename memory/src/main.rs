3527281a88604e343a4a4a7798ebd625ac453780
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-10-08 07:48:00 +0330

    getting refrences of strcut members

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,9 +1,37 @@
+use std::fmt::Display;
+
+enum Project {
+    ProjectA,
+    ProjectB,
+}
+
+impl Display for Project {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Project::ProjectA => write!(f, "ProjectA"),
+            Project::ProjectB => write!(f, "ProjectB"),
+        }
+    }
+}
+
+struct  Mission {
+    name: String,
+    project: Project,
+}
 
 fn main() {
-    let mut x: u8 = 5;
-    let y: &mut u8 = &mut x;
+    let mut mission = Mission {
+        name: String::from("Mission 1"),
+        project: Project::ProjectA,
+    };
 
-    *y += 1;
+    let _name = &mut mission.name;
+    let _project = &mut mission.project;
 
-    println!("x = {}", x);
+    _name.push_str(" - Updated");
+    *_project = Project::ProjectB;
+
+    // print mission
+    println!("Mission: {}", mission.name);
+    println!("Project: {}", mission.project);
 }
