1e46bd1a7a4558cd50eecc16bf763ba951e2c048
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-10-09 17:52:40 +0330

    feat: add initial macro

---
+++ macro_rules/src/lib.rs
@@ -0,0 +1,44 @@
+
+// This macro is created based on a crust of rust epsiode by jonhoo. 
+#[macro_export]
+macro_rules! avec {
+    () => {
+        Vec::new()
+    };
+    ($($x:expr),* $(,)?) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn creates_empty_vec() {
+        let v: Vec<i32> = avec![];
+        assert!(v.is_empty());
+    }
+
+    #[test]
+    fn creates_vec_wit_two_items() {
+        let v: Vec<i32> = avec![1, 2];
+        assert!(v.len() == 2);
+        assert!(v[0] == 1);
+        assert!(v[1] == 2);
+
+    }
+    
+    #[test]
+    fn creates_vec_and_accepts_trailing_comma() {
+        let v = avec![1, 2,];
+        assert!(v.len() == 2);
+        assert!(v[0] == 1);
+        assert!(v[1] == 2);
+    }
+}
