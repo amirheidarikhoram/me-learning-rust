6a28e1c91e060efd6fede7d3ac4293fe6e88e19f
Amir Heidari <amir.heidari.khoram@gmail.com>
2022-10-27 21:58:26 +0330

    impl mpsc example to show unordered results

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,6 +1,16 @@
-use std::{sync::{Mutex, Arc}, thread};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
+    // create_threads_arc_mutes();
+
+    create_threads_mpsc();
+}
+
+#[allow(dead_code)]
+fn create_threads_arc_mutes() {
     let a = Arc::new(Mutex::new(0));
 
     let cloned_a = a.clone();
@@ -17,4 +27,57 @@ fn main() {
     let value = a.lock().unwrap();
 
     println!("Hello, world! {}", value);
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Message {
+    content: String,
+    thread_id: u32,
+}
+
+#[allow(dead_code)]
+fn create_threads_mpsc() {
+    let mut handles = vec![];
+
+    let (send, recv) = std::sync::mpsc::channel::<Message>();
+
+    let mut cur = 0;
+    static MAX_THREAD: u8 = 10;
+
+    loop {
+        let cloned_send = send.clone();
+        let handle = thread::spawn(move || {
+            let message = Message {
+                content: String::from("Hello"),
+                thread_id: cur,
+            };
+
+            cloned_send.send(message).unwrap();
+        });
+
+        handles.push(handle);
+
+        cur += 1;
+        if cur == MAX_THREAD.into() {
+            break;
+        }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    cur = 0;
+
+    loop {
+        let message = recv.recv().unwrap();
+        println!("Message: {:?}", message);
+
+        cur += 1;
+        if cur == MAX_THREAD.into() {
+            break;
+        }
+    }
+
 }
